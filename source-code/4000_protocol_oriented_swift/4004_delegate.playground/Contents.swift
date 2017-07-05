ab043d953bf4e168bf81c3d710bb35b502f789f8
Bob Lee <bob@bobthedeveloper.io>
2017-07-06 00:05:15 +0900

    Added delegate

--- source-code/4000_protocol_oriented_swift/4004_delegate.playground/Contents.swift
+++ source-code/4000_protocol_oriented_swift/4004_delegate.playground/Contents.swift
@@ -4,13 +4,15 @@
  ### Protocol Oriented Swift
  ### Delegate
  
- **Problem:** Communicate between classes and structs
+ **Problem:** How does delegate even work?
  
  ---
  */
-//: Delegate
-//: 1. A gift from Apple engineers
-//: 2. microwave analogy
+
+//: > **Purpose of Delegate:** Communicate between objects
+
+
+//: **Goal:** Send data from `FirstVC` to `SecondVC`
 
 //: Design Protocol
 protocol PassDataDelegate {
@@ -20,16 +22,17 @@ protocol PassDataDelegate {
 
 //: Design Delegator (Sender)
 class FirstVC {
+  func drawCircle() {}
   var delegate: PassDataDelegate?
 }
 
-FirstVC().delegate?.passData(data: "Bob")
+FirstVC().delegate?.passData(data: "A bunch of contracts")
 
 
 //: Design Delegate (Receiver)
 class SecondVC: PassDataDelegate {
   func passData(data: String) {
-    print("Something happened")
+    print("The CEO gave me \(data)")
   }
 }
 
@@ -40,17 +43,32 @@ let secondVC = SecondVC()
 
 //: Assign Delegate
 firstVC.delegate = secondVC
-firstVC.delegate?.passData(data: "Hello, 1231231")  
+firstVC.delegate?.passData(data: "A bunch of contracts")
 
 
 
 
+//: Practical Example in iOS
+import UIKit
+
+class BobViewController: UIViewController, UITableViewDelegate {
+  let tableView = UITableView()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableView.delegate = self
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print(indexPath)
+  }
+}
 
 
+//: tableView.delegate.tableView(_ tableView: UITableView, didSelectRorwAt indexPath: IndexPath)
 
 
-
-
+// UITableView is the delegator/CEO/sender
+// BobViewController is the delegate/Secretary/receiver
 
 
 
