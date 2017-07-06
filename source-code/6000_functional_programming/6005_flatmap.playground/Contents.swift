0e38e5fc89a5a4b661613e00a6dfe7c3fe8dfd0b
Bob Lee <bob@bobthedeveloper.io>
2017-07-06 16:00:14 +0900

    Semi-prepped for Flatmap

--- source-code/6000_functional_programming/6005_flatmap.playground/Contents.swift
+++ source-code/6000_functional_programming/6005_flatmap.playground/Contents.swift
@@ -10,10 +10,38 @@
  */
 
 
+let example = [[5,2,7],[4,8],[9,1,3]]
+let result = example.flatMap { $0 }
+print(result)
+
+let anotherExample: [Int?] = [1, 2, nil, 3, 4, 5, nil, nil]
+let AnotherRsult = anotherExample.flatMap { $0 }
+
+
+func myFlatMap(numbers: [Int]) {
+  
+}
+
+
+func bobFlatMap(numbers: [[Int]], operation: (Int) -> Int) {
+  var result: [Int] = []
+  for array in numbers {
+    for item in array {
+      result.append(item)
+    }
+  }
+  print(result)
+}
+
+
+bobFlatMap(numbers: [[1, 2, 3], [4, 5], [6, 7, 8]], operation: { $0 })
+bobFlatMap(numbers: [[1, 2, 3, 4, 5]], operation: { $0 })
+
+
 
 
 extension Array {
-  func flatMap<U>(transform: (Element) -> U?) -> [U] {
+  func myFlatMap<U>(transform: (Element) -> U?) -> [U] {
     var result = [U]()
     for item in map(transform) {
       if let nonOptional = item {
@@ -24,23 +52,12 @@ extension Array {
   }
 }
 
+let closure: (Int) -> Int? = { $0 }
 
 
-
-
-
-
-
-
-
-let collections = [[5,2,7],[4,8],[9,1,3]]
-let flat = collections.flatMap { $0 }
-print(flat)
-
-
-let onlyEvenSimpler = collections.flatMap { $0.filter { $0 % 2 == 0 } }
-
-
+let myResult = [[1], [2], [3], nil].myFlatMap { $0 }
+let flattenCollection = myResult.joined()
+let flattenArray = Array(flattenCollection)
 
 
 
